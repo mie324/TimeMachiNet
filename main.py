4fda20043699503cd92758a7791ffeb35d2f2a18
Anup Deb <anupdeb@Anups-MacBook-Pro.local>
2018-11-11 13:20:29 -0500

    Added imports

--- main.py
+++ main.py
@@ -1,3 +1,25 @@
 """
 This will contain the training loop
 """
+
+
+import torch
+import torch.optim as optim
+
+import torchtext
+from torchtext import data
+import spacy
+
+import argparse
+import os
+
+
+from models import *
+
+
+
+gc.collect()
+
+
+
+
