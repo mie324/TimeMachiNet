ac7b18c41e4e6b77b1941c280135fa3e6b6dfccb
Steve Kim <woosik.kim@mail.utoronto.ca>
2018-11-11 15:07:52 -0500

    populated models

--- main.py
+++ main.py
@@ -14,7 +14,7 @@ import argparse
 import os
 
 
-from models import *
+from model import *
 
 
 
@@ -22,4 +22,76 @@ gc.collect()
 
 
 
+if __name__ == "__main__":
+	parser = argparse.ArgumentParser(formatter_class=argparse.RawTextHelpFormatter)
+	parser.add_argument('--mode', choices=['train', 'test'], default='train')
 
+	# Arguments for Training
+	parser.add_argument('--epochs', default=1, type=int)
+	parser.add_argument('--batch-size', dest='batch_size', default=64, type=int)
+    parser.add_argument('--weight-decay', default=1e-5, type=float)
+    parser.add_argument('--learning-rate', dest='learning_rate', default=2e-4, type=float)
+    parser.add_argument('--b1', dest='b1', default=0.5, type=float)
+    parser.add_argument('--b2', dest='b2', default=0.999, type=float)
+    parser.add_argument('--shouldplot', dest='sp', default=False, type=bool)
+
+    # Arguments for Testing
+    parser.add_argument('--age', required=False, type=int)
+    parser.add_argument('--gender', required=False, type=int)
+    parser.add_argument('--watermark', action='store_true')
+
+    # General Arguments
+    parser.add_argument('--load', required=False, default=None, help='Trained models path for pre-training or for testing')
+    parser.add_argument('--input', default=None)
+    parser.add_argument('--output', default='')
+    parser.add_argument('-z', dest='z_channels', default=50, type=int, help='Length of Z vector')
+    args = parser.parse_args()
+
+    consts.NUM_Z_CHANNELS = args.z_channels
+    net = model.Network()
+
+
+    if args.mode == 'train':
+
+    	# Initialize
+    	if args.load is None:
+    		b = (args.b1, args.b2)
+    		weight_decay = args.weight_decay
+    		lr = args.learning_rate
+    	else:
+    		b = None
+    		weight_decay = None
+    		lr = None
+    		net.load(args.load)
+    		print("hi")
+
+    	data_set = args.input
+    	print("Data folder loaded lit")
+    	output_dest = args.output
+    	os.makedirs(output_dest, exist_ok=True)  # if there isn't already a directory, make a new one
+    	print("Output folder is lit")
+
+    	net.train(
+
+    		)
+
+    elif args.mode == 'test':
+
+    	if args.load is None:
+    		raise RuntimeError("yo common give us some trained models")
+
+    	net.load(path=args.load, slim=True)
+
+    	output_dest = args.output
+
+    	if not os.path.isdir(output_dest):  # if there isn't already a directory, make a new one
+    		os.makedirs(output_dest)
+
+    	image_tensor = tensor_transform(loader(args.input)).to(net.device)
+    	net.test_image(
+    		image_tensor=image_tensor,
+    		age=args.age,
+    		gender=args.gender,
+    		target=output_dest,
+    		watermark=args.watermark
+    	)
--- model.py
+++ model.py
@@ -21,53 +21,115 @@ from torch.utils.data import DataLoader
 
 class Encoder(nn.Module):
 
-	def __init__(self):
-		pass
+    def __init__(self):
+        super(Encoder, self).__init__()
+        num_conv_layers = 6
 
-	def add_conv(layer_list, name, input, output, kernel_size, stride, padding, activation_fcn):
-		pass
+        self.conv_layers = nn.ModuleList()
 
-	def forward(self, face):
-		pass
+        def add_conv(layer_list, name, input, output, kernel_size, stride, activation_fcn):
+            return layer_list.add_module(
+                name,
+                nn.Sequential(
+                    nn.Conv2d(
+                        in_channels=input,
+                        out_channels=output,
+                        kernel_size=kernel_size,
+                        stride=stride,
+                    ),
+                    activation_fcn
+                )
+            )
+        add_conv(self.conv_layers, 'e_conv_1', input=3, output=64, kernel_size=2, stride=2, activation_fcn=nn.ReLU())
+        add_conv(self.conv_layers, 'e_conv_2', input=64, output=128, kernel_size=2, stride=2, activation_fcn=nn.ReLU())
+        add_conv(self.conv_layers, 'e_conv_3', input=128, output=256, kernel_size=2, stride=2, activation_fcn=nn.ReLU())
+        add_conv(self.conv_layers, 'e_conv_4', input=256, output=512, kernel_size=2, stride=2, activation_fcn=nn.ReLU())
+        add_conv(self.conv_layers, 'e_conv_5', input=512, output=1024, kernel_size=2, stride=2, activation_fcn=nn.ReLU())
+
+        self.fc_1 = nn.Sequential(OrderedDict([
+            ('e_fc_1', nn.Linear(
+                in_features=1024 * 4 * 4,
+                out_features=consts.NUM_Z_CHANNELS
+            )),
+            ('tanh_1', nn.Tanh())
+        ]))
+
+    def forward(self, face):
+        x = face
+        for conv_layer in self.conv_layers:
+            x = conv_layer(x)
+        x = x.view(x.size(0), -1)
+        x = self.fc_1(x)
+        return x
+
 
 class DiscriminatorVec(nn.Module):
-	def __init__(self):
-		pass
+    def __init__(self):
+        super(DiscriminatorVec, self).__init__()
+        dims = (consts.NUM_Z_CHANNELS, consts.NUM_ENCODER_CHANNELS, consts.NUM_ENCODER_CHANNELS // 2,
+                consts.NUM_ENCODER_CHANNELS // 4)
+        self.layers = nn.ModuleList()
+        for i, (in_dim, out_dim) in enumerate(zip(dims[:-1], dims[1:]), 1):
+            self.layers.add_module(
+                'dvec_fc_%d' % i,
+                nn.Sequential(
+                    nn.Linear(in_dim, out_dim),
+                    nn.BatchNorm1d(out_dim),
+                    nn.ReLU()
+                )
+            )
 
-	def forward(self, vector):
-		pass
+        self.layers.add_module(
+            'dvec_fc_%d' % (i + 1),
+            nn.Sequential(
+                nn.Linear(out_dim, 1),
+            )
+        )
+
+    def forward(self, vector):
+        output = vector
+        for layer in self.layers:
+            output = layer(output)
+        return output
+
 
 class DiscriminatorImg(nn.Module):
-	def __init__(self):
-		pass
+    def __init__(self):
+        pass
 
-	def forward(self, images, labels, device):
-		pass
+    def forward(self, images, labels, device):
+        pass
 
 
 class Generator(nn.Module):
-	def __init__(self):
-		pass
+    def __init__(self):
+        pass
 
-	def forward(self, vector, age=None, gender=None):
-		pass
+    def forward(self, vector, age=None, gender=None):
+        pass
 
-	def add_deconv(name, input_dim, output_dim, kernel_size, stride, activation_fcn):
-		pass
+    def add_deconv(name, input_dim, output_dim, kernel_size, stride, activation_fcn):
+        pass
 
-	def decomp(self, x):
-		pass
+    def decomp(self, x):
+        pass
+
 
 class Network(object):
-	def __init__(self):
-		pass
+    def __init__(self):
+        pass
 
-	def __call__(self, *args, **kwargs): 
-		pass
+    def __call__(self, *args, **kwargs):
+        pass
 
-	def __repr__(self):
-		pass
+    def __repr__(self):
+        pass
 
-	def test_image(self, image_tensor, age, gender, target, watermark):
-		pass
-	
+    def test_image(self, image_tensor, age, gender, target, watermark):
+        pass
+
+    def train(self):
+        pass
+
+    def load(self):
+        pass
