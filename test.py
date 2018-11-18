0ad2d2f4a583c971c3d00f64f345ab61d2707345
Anup Deb <debovik01@gmail.com>
2018-11-18 17:58:36 -0500

    Implemented script to run generator on single input image

---
+++ test.py
@@ -0,0 +1,50 @@
+import torch
+import argparse
+import os
+from torch.autograd import Variable
+import torchvision.transforms as transforms
+import torchvision.datasets as data
+import torchvision.utils as vutils
+
+def test(args):
+    output = args.output
+    if not os.path.exists(output):
+        os.mkdir(output)
+
+    netG = torch.load(args.pretrained_generator)
+    netE = torch.load(args.pretrained_encoder)
+
+    val_l = -torch.ones(10 * 10).view(10, 10)
+    for i, l in enumerate(val_l):
+        l[1 // 8] = 1
+    val_l = Variable(val_l)
+    img = data.ImageFolder(root=args.input,
+                               transform=transforms.Compose([
+                                   transforms.Resize(128),
+                                   transforms.ToTensor(),
+                                   transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5)),
+                               ]))
+    dataloader = torch.utils.data.DataLoader(img,
+                                            batch_size=1,
+                                            shuffle=True)
+    for i, (img, label) in enumerate(dataloader):
+        val_z = netE(Variable(img.repeat(10,1,1,1)))
+        gender_var = Variable(torch.Tensor(args.gender))
+        val_gender_var = Variable(gender_var[:1].view(-1, 1).repeat(10, 1))
+        val_gen = netG(val_z, val_l, val_gender_var)
+        vutils.save_image(val_gen.data,
+                          "{}/test.png".format(args.output),
+                          normalize=True)
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--pretrained-generator', type=str, default="models/generator_epoch_2.pt")
+    parser.add_argument('--pretrained-encoder', type=str, default="models/encoder_epoch_2.pt")
+    parser.add_argument('--input', type=str, default="test")
+    parser.add_argument('--output', type=str, default='output')
+    parser.add_argument('--gender', type=int, default=1)
+    parser.add_argument('--age', type=int, default=25)
+
+    args = parser.parse_args()
+
+    test(args)
