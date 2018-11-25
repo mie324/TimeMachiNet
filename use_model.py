f8c773e2039b6197791b8e44741e8f03d90dd6a3
Anup Deb <debovik01@gmail.com>
2018-11-25 13:08:24 -0500

    Added evaluate function

--- use_model.py
+++ use_model.py
@@ -6,13 +6,44 @@ import torchvision.transforms as transforms
 import torchvision.datasets as data
 import torchvision.utils as vutils
 
-pretrained_generator = "models/generator_epoch_49.pt"
-pretrained_encoder = "models/encoder_epoch_49.pt"
+pretrained_encoder = "./models/encoder_epoch_49"
+pretrained_generator = "./models/generator_epoch_49"
+input = "./test/input"
+output_model = "./test/output/output.png"
+output_orig = "./test/output/input_img.png"
 
-def run_model():
+def evaluate():
+    netG = torch.load(pretrained_encoder, map_location="cpu")
+    netE = torch.load(pretrained_generator, map_location="cpu")
 
+    val_l = -torch.ones(10 * 10).view(10, 10)
+    for i, l in enumerate(val_l):
+        l[i // 1] = 1
+    val_l = Variable(val_l)
+    img = data.ImageFolder(root=input,
+                           transform=transforms.Compose([
+                               transforms.Resize(128),
+                               transforms.CenterCrop(128),
+                               transforms.ToTensor(),
+                               transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5)),
+                           ]))
+    dataloader = torch.utils.data.DataLoader(img,
+                                             batch_size=1,
+                                             shuffle=True)
+    for i, (img, label) in enumerate(dataloader):
+        input_img = Variable(img)
+        vutils.save_image(val_gen.data,
+                          output_orig,
+                          normalize=True)
+        val_z = netE(Variable(img.repeat(10, 1, 1, 1)))
+        gender_var = Variable(torch.Tensor(args.gender))
+        val_gender_var = Variable(gender_var[:1].view(-1, 1).repeat(10, 1))
+        val_gen = netG(val_z, val_l, val_gender_var)
+        vutils.save_image(val_gen.data,
+                          output_model,
+                          normalize=True)
 
-
+        
 def test(args):
     output = args.output
     if not os.path.exists(output):
